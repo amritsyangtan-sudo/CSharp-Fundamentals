8738e22fcb9fde39a7907493b52d3e4e256f1a72
AmritS <amritsyangtan1@gmail.com>
2026-07-12 13:38:32 +0545

    concurrency vs parallesim

--- Program.cs
+++ Program.cs
@@ -1,20 +1,31 @@
 ﻿using System.Runtime.InteropServices.Marshalling;
 using System.Security.Cryptography;
+using System.Net.Http;
 
 namespace CSharp_Fundamentals;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        
+        HttpClient client = new HttpClient();
+
+        Console.WriteLine("Starting download...");
+
+        Task<string> downloadTask =
+            client.GetStringAsync("https://www.google.com");
+
+        // While download is happening...
+        for (int i = 1; i <= 20; i++)
+        {
+            Console.Write(".");
+            await Task.Delay(200);
+        }
+
+        string html = await downloadTask;
+
+        Console.WriteLine();
+        Console.WriteLine($"Downloaded {html.Length} characters.");
     }
 }
 
-class Factory<T>
-{
-    public T Create(T value)
-    {
-        return value;
-    }
-}
